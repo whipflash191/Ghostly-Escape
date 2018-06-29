e1c62b54cfa4106b333f0c8b58e45872bf9894ef
Dylan Faith <whipflash191@gmail.com>
2018-06-29 12:22:11 +1000

    Current Progress
    Has Player Controls & WIP Scripts for ghosts.

---
+++ Assets/Scriptable Objects/Ghost.cs
@@ -0,0 +1,22 @@
+﻿/*
+* Copyright (c) Dylan Faith (Whipflash191)
+* https://twitter.com/Whipflash191
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Ghost", menuName = "Ghost")]
+public class Ghost : ScriptableObject {
+    public Sprite ghost;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,40 @@
+﻿/*
+* Copyright (c) Dylan Faith (Whipflash191)
+* https://twitter.com/Whipflash191
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public bool canControl = true;
+    [Range(0,4)]
+    public float movementSpeed;
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        if (canControl)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, 1, 0) * (Time.deltaTime * movementSpeed));
+            } else if(Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, -1, 0) * (Time.deltaTime * movementSpeed));
+            } else if(Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-1, 0, 0) * (Time.deltaTime * movementSpeed));
+            } else if(Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(1, 0, 0) * (Time.deltaTime * movementSpeed));
+            }
+        }
+	}
+}
