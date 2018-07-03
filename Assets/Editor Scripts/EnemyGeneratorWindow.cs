baccceb2f021ac5932551f18626ed490f482955f
Dylan Faith <whipflash191@gmail.com>
2018-07-03 23:48:44 +1000

    W.I.P Ghost & Enemy Generators

---
+++ Assets/Editor Scripts/EnemyGeneratorWindow.cs
@@ -0,0 +1,113 @@
+﻿/*
+* Copyright (c) Dylan Faith (Whipflash191)
+* https://twitter.com/Whipflash191
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEditor;
+
+public class EnemyGeneratorWindow : EditorWindow
+{
+    int enemyLayer = 0;
+    int enemyWaypoints = 0;
+    string enemyName = "Replace This";
+    Sprite enemySprite = null;
+    bool isPrefab = false;
+    bool canMake = true;
+    float mvmSpeed = 4f;
+    Vector3 enemyScale = new Vector3(1, 1, 1);
+
+    [MenuItem("Window/Enemy Generator")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<EnemyGeneratorWindow>("Ennemy Generator");
+    }
+
+    private void OnGUI()
+    {
+        enemyName = EditorGUILayout.TextField("Enemy Name", enemyName, EditorStyles.textField);
+        DuplicateCheck();
+        enemySprite = (Sprite)EditorGUILayout.ObjectField(enemySprite, typeof(Sprite), false);
+        enemyScale = EditorGUILayout.Vector3Field("Object Scale", enemyScale);
+        enemyLayer = EditorGUILayout.LayerField("Object Layer", enemyLayer);
+        isPrefab = EditorGUILayout.Toggle("Make Prefab", isPrefab, EditorStyles.toggle);
+        mvmSpeed = EditorGUILayout.Slider("Movement Speed", mvmSpeed, 0, 4);
+        EditorGUILayout.BeginHorizontal();
+        EditorGUI.BeginDisabledGroup(canMake == false);
+        if (GUILayout.Button("Make Enemy"))
+        {
+            MakeEnemy();
+        }
+        EditorGUI.EndDisabledGroup();
+        if (GUILayout.Button("Reset"))
+        {
+            ResetFields();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void MakeEnemy()
+    {
+        /*
+         * Function to make the Enemy Object or Prefab
+         * Adds compnents required & player scripts needed
+         * Adds waypoints used for the AI
+         * Specifies Layer to add too
+        */
+        GameObject newEnemy = new GameObject(enemyName);
+        newEnemy.transform.localScale = enemyScale;
+        newEnemy.layer = enemyLayer;
+        newEnemy.AddComponent<SpriteRenderer>();
+        newEnemy.GetComponent<SpriteRenderer>().sprite = enemySprite;
+        newEnemy.AddComponent<PolygonCollider2D>();
+        newEnemy.AddComponent<NavMeshAgent>();
+        newEnemy.GetComponent<NavMeshAgent>().speed = mvmSpeed;
+        for (int i = 0; i < enemyWaypoints; i++)
+        {
+            GameObject tempWaypoint = new GameObject("waypoint" + i.ToString());
+            tempWaypoint.transform.SetParent(newEnemy.transform);
+        }
+        if (isPrefab)
+        {
+            PrefabUtility.CreatePrefab("Assets/Prefabs/Enemies/" + enemyName + ".prefab", newEnemy);
+            GameObject.DestroyImmediate(newEnemy);
+        }
+        ResetFields();
+    }
+
+    private void DuplicateCheck()
+    {
+        if (GameObject.Find(enemyName) != null)
+        {
+            EditorGUILayout.LabelField("Name already exists in scene!", EditorStyles.boldLabel);
+            canMake = false;
+        }
+        else if (AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Enemies/" + enemyName + ".prefab", typeof(GameObject)) != null)
+        {
+            EditorGUILayout.LabelField("A Prefab with this name already exists!", EditorStyles.boldLabel);
+            canMake = false;
+        }
+        else if (enemyName == "Replace This" || enemyName == "")
+        {
+            canMake = false;
+        }
+        else
+        {
+            canMake = true;
+        }
+    }
+
+    private void ResetFields()
+    {
+        enemyName = "Replace This";
+        enemyWaypoints = 0;
+        enemyScale = new Vector3(1, 1, 1);
+        enemyLayer = 0;
+        enemySprite = null;
+        isPrefab = false;
+        mvmSpeed = 4f;
+    }
+}
---
+++ Assets/Editor Scripts/GhostGeneratorWindow.cs
@@ -0,0 +1,117 @@
+﻿/*
+* Copyright (c) Dylan Faith (Whipflash191)
+* https://twitter.com/Whipflash191
+*/
+
+using UnityEngine;
+using UnityEditor;
+
+public class GhostGeneratorWindow : EditorWindow
+{
+    int ghostLayer = 0;
+    string ghostName = "Replace This";
+    Sprite ghostSprite = null;
+    bool isPlayer = false;
+    bool isPrefab = false;
+    bool canMake = true;
+    float mvmSpeed = 4f;
+    Vector3 ghostScale = new Vector3(1,1,1);
+
+    [MenuItem("Window/Ghost Generator")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow<GhostGeneratorWindow>("Ghost Generator");
+    }
+
+    private void OnGUI()
+    {
+        ghostName = EditorGUILayout.TextField("Ghost Prefab Name", ghostName, EditorStyles.textField);
+        DuplicateCheck();
+        ghostSprite = (Sprite)EditorGUILayout.ObjectField(ghostSprite, typeof(Sprite), false);
+        ghostScale = EditorGUILayout.Vector3Field("Object Scale", ghostScale);
+        ghostLayer = EditorGUILayout.LayerField("Object Layer", ghostLayer);
+        isPrefab = EditorGUILayout.Toggle("Make Prefab", isPrefab, EditorStyles.toggle);
+        isPlayer = EditorGUILayout.Toggle("Is Player", isPlayer, EditorStyles.toggle);
+        if(isPlayer)
+        {
+            EditorGUILayout.BeginFadeGroup(1);
+            EditorGUI.indentLevel++;
+            mvmSpeed = EditorGUILayout.Slider("Movement Speed", mvmSpeed, 0, 4);
+            EditorGUI.indentLevel--;
+        } else
+        {
+            EditorGUILayout.EndFadeGroup();
+        }
+        EditorGUILayout.BeginHorizontal();
+        EditorGUI.BeginDisabledGroup(canMake == false);
+        if(GUILayout.Button("Make Ghost"))
+        {
+            MakeGhost();
+        }
+        EditorGUI.EndDisabledGroup();
+        if(GUILayout.Button("Reset"))
+        {
+            ResetFields();
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void MakeGhost()
+    {
+        /*
+         * Function to make the Ghost Object or Prefab
+         * Adds compnents required & player scripts needed
+         * Adds to player layer or sprite layer
+        */
+        GameObject newGhost = new GameObject(ghostName);
+        newGhost.transform.localScale = ghostScale;
+        newGhost.layer = ghostLayer;
+        newGhost.AddComponent<SpriteRenderer>();
+        newGhost.GetComponent<SpriteRenderer>().sprite = ghostSprite;
+        newGhost.AddComponent<PolygonCollider2D>();
+        if(isPlayer)
+        {
+            newGhost.AddComponent<PlayerControl>();
+            newGhost.GetComponent<PlayerControl>().movementSpeed = mvmSpeed;
+        }
+        if(isPrefab)
+        {
+            PrefabUtility.CreatePrefab("Assets/Prefabs/Ghosts/" + ghostName + ".prefab", newGhost);
+            GameObject.DestroyImmediate(newGhost);
+        }
+        ResetFields();
+    }
+
+    private void DuplicateCheck()
+    {
+        if (GameObject.Find(ghostName) != null)
+        {
+            EditorGUILayout.LabelField("Name already exists in scene!", EditorStyles.boldLabel);
+            canMake = false;
+        }
+        else if (AssetDatabase.LoadAssetAtPath("Assets/Prefabs/Ghosts/" + ghostName + ".prefab", typeof(GameObject)) != null)
+        {
+            EditorGUILayout.LabelField("A Prefab with this name already exists!", EditorStyles.boldLabel);
+            canMake = false;
+        }
+        else if (ghostName == "Replace This" || ghostName == "")
+        {
+            canMake = false;
+        }
+        else
+        {
+            canMake = true;
+        }
+    }
+
+    private void ResetFields()
+    {
+        ghostName = "Replace This";
+        ghostLayer = 0;
+        ghostSprite = null;
+        ghostScale = new Vector3(1, 1, 1);
+        isPlayer = false;
+        isPrefab = false;
+        mvmSpeed = 4f;
+    }
+}
