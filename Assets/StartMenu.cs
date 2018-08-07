f3e7d24a1efd87d1683897853741deb89c558747
Dylan Faith <whipflash191@gmail.com>
2018-08-07 20:07:55 +1000

    Implemented StartMenu

--- Assets/StartMenu.cs
+++ Assets/StartMenu.cs
@@ -7,13 +7,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class StartMenu : MonoBehaviour {
+    public Slider master;
+    public Slider footstep;
+    public Slider background;
+    public Dropdown quality;
     public Animator pressHere;
+    public GameObject settings;
     public GameObject menu;
+    public AudioMixer mixer;
 	// Use this for initialization
 	void Start () 
 	{
+        if(PlayerPrefs.GetString("CustomSettings") == "true")
+        {
+            LoadSettings();
+        }
         pressHere.speed = 0.4f;
 	}
 	
@@ -22,8 +34,14 @@ public class StartMenu : MonoBehaviour {
 	{
 		
 	}
+    
+    public void RevealSettings()
+    {
+        menu.SetActive(false);
+        settings.SetActive(true);
+    }
 
-   public void RevealMenu()
+    public void RevealMenu()
     {
         menu.SetActive(true);
     }
@@ -37,4 +55,40 @@ public class StartMenu : MonoBehaviour {
     {
         QualitySettings.SetQualityLevel(qualityIndex);
     }
+
+    public void SetMasterVolume(float volume)
+    {
+        mixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetFootstepVolume(float volume)
+    {
+        mixer.SetFloat("footstepVolume", volume);
+    }
+
+    public void SetBackgroundVolume(float volume)
+    {
+        mixer.SetFloat("bgVolume", volume);
+    }
+
+    public void SaveSettings()
+    {
+        settings.SetActive(false);
+        menu.SetActive(true);
+        PlayerPrefs.SetString("CustomSettings", "true");
+        PlayerPrefs.SetFloat("MasterVol", master.value);
+        PlayerPrefs.SetFloat("BackgroudVol", background.value);
+        PlayerPrefs.SetFloat("FootstepVol", footstep.value);
+        PlayerPrefs.SetInt("Quality", quality.value);
+    }
+
+    public void LoadSettings()
+    {
+        PlayerPrefs.GetString("CustomSettings");
+        PlayerPrefs.GetFloat("MasterVol");
+        PlayerPrefs.GetFloat("BackgroudVol");
+        PlayerPrefs.GetFloat("FootstepVol");
+        PlayerPrefs.GetInt("Quality");
+    }
+
 }
